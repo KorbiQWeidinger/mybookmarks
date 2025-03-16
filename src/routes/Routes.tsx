9e31376dc20f5e88fd125c79244026c9a217645d
Korbi <korbi.weidinger2@gmail.com>
2025-03-16 16:56:49 +0100

    refactor: remove settings page and keep only settings modal (ai)

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,14 +1,12 @@
 import { HashRouter as Router, Route, Routes as RouterRoutes } from 'react-router-dom';
 import { BookmarksPage } from '@/pages/bookmarks-page';
 import { LandingPage } from '@/pages/landing-page';
-import { SettingsPage } from '@/pages/settings-page';
 
 export const Routes = () => {
   return (
     <Router>
       <RouterRoutes>
         <Route path='/landing' element={<LandingPage />} />
-        <Route path='/settings' element={<SettingsPage />} />
         <Route path='/' element={<BookmarksPage />} />
       </RouterRoutes>
     </Router>
